aaaa1d6bd4475957898bf2dc0b99f5cc45eab432
murphylee2048 <1134697131@qq.com>
2019-07-17 21:17:12 +0800

    [ML]feat : findAllCase

--- src/main/java/com/tw/apistackbase/controller/CaseController.java
+++ src/main/java/com/tw/apistackbase/controller/CaseController.java
@@ -8,15 +8,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CaseController {
     @Autowired
     private CaseInterface caseInterface;
 
     @GetMapping("/cases/{caseId}")
-    public Case findById(@PathVariable int caseId) {
+    public Case findCaseById(@PathVariable int caseId) throws Exception{
         return caseInterface.findById(caseId).get();
     }
 
+    @GetMapping("/cases")
+    public List<Case> findAllCase() throws Exception {
+        return caseInterface.findAll();
+    }
 
 }
