7fe114a7ef0fec0fde9d5a6b031864d9b3ea75c2
murphylee2048 <1134697131@qq.com>
2019-07-17 19:15:51 +0800

    [ML]feat:class Cases

---
+++ src/main/java/com/tw/apistackbase/model/Cases.java
@@ -0,0 +1,22 @@
+package com.tw.apistackbase.model;
+
+public class Cases {
+    private int caseId;
+    private String caseName;
+
+    public int getCaseId() {
+        return caseId;
+    }
+
+    public String getCaseName() {
+        return caseName;
+    }
+
+    public void setCaseId(int caseId) {
+        this.caseId = caseId;
+    }
+
+    public void setCaseName(String caseName) {
+        this.caseName = caseName;
+    }
+}
